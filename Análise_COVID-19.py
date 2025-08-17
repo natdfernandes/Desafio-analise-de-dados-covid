b7cfd60e35e45e2ca580fd8b51571919cd2d7760
Natalia <nataliadiasfernande@gmail.com>
2025-08-17 17:23:46 -0300

    ajustando texto

--- Análise_COVID-19.py
+++ Análise_COVID-19.py
@@ -7,7 +7,7 @@ st.title("Análise COVID-19")
 
 st.markdown(
     """
-    Este dashboard interativo apresenta uma análise abrangente da evolução da pandemia de COVID-19 nos Estados Unidos, com base nos dados públicos disponibilizados pelo *The New York Times* ([GitHub - NYT COVID-19 Data](https://github.com/nytimes/covid-19-data)).
+    Este dashboard interativo apresenta uma análise abrangente da evolução da pandemia de COVID-19, com base nos dados públicos disponibilizados pelo *The New York Times* ([GitHub - NYT COVID-19 Data](https://github.com/nytimes/covid-19-data)).
 
     A plataforma oferece visualizações dinâmicas e filtros personalizados que permitem explorar:
 
