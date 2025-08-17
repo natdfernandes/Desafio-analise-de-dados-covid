36def1c526b59d822fff76b1570ecd0b11c533ca
Natalia <nataliadiasfernande@gmail.com>
2025-08-17 14:35:28 -0300

    Organizando código

--- pages/1_Óbitos_em_Excesso.py
+++ pages/1_Óbitos_em_Excesso.py
@@ -6,41 +6,43 @@ deaths_header = "deaths"
 year_header = "year"
 filter_by_country_key = "filter_by_country"
 
-st.title("Óbitos em Excesso Durante a Pandemia de Coronavírus")
 
-with st.expander("Sobre está página"):
-    st.markdown(
+def display_about_content():
+    st.title("Óbitos em Excesso Durante a Pandemia de Coronavírus")
+
+    with st.expander("Sobre está página"):
+        st.markdown(
+            """
+        Este painel apresenta dados sobre **óbitos em excesso** registrados durante a pandemia de COVID-19, com base no levantamento do *The New York Times*. Os dados incluem mortes por todas as causas em 32 países, permitindo uma análise mais ampla do verdadeiro impacto da pandemia além dos números oficiais de óbitos por COVID-19.
+
+        Os dados foram coletados de registros civis, departamentos de saúde e outras fontes oficiais. Como nem todas as mortes relacionadas à COVID-19 foram oficialmente registradas (por falta de testes ou óbitos fora de hospitais), o número de **óbitos em excesso** — diferença entre mortes registradas e o número esperado com base em anos anteriores — oferece um retrato mais completo.
+
+        ### O que você encontrará nesta seção:
+
+        * Dados por país e, em alguns casos, por cidade.
+        * Informações organizadas por semana ou mês.
+        * Comparação entre mortes reais e mortes esperadas.
+        * Base de cálculo construída a partir de médias históricas (2010–2019, dependendo do país).
+        * Tabelas com colunas como: país, localidade, datas, número de óbitos, óbitos esperados e excedentes.
+
+        ### Metodologia:
+
+        * O número de óbitos esperados foi calculado com modelos estatísticos que consideram tendências demográficas e variações sazonais.
+        * As análises podem ser afetadas por atrasos ou falhas nos sistemas de registro, especialmente em países com menor cobertura estatística.
+        * Alguns estados e países foram excluídos por inconsistência ou falta de dados.
+
+        ### Fontes e Licença:
+
+        * Os dados foram coletados e organizados por jornalistas e especialistas do *The New York Times*, com o apoio de pesquisadores e demógrafos.
+        * Estão disponíveis para uso público não comercial, desde que seja feita a devida atribuição ao NYT.
+        * Para mais informações, acesse: [Link para o gráfico interativo original](https://www.nytimes.com/interactive/2020/04/21/world/coronavirus-missing-deaths.html)
         """
-    Este painel apresenta dados sobre **óbitos em excesso** registrados durante a pandemia de COVID-19, com base no levantamento do *The New York Times*. Os dados incluem mortes por todas as causas em 32 países, permitindo uma análise mais ampla do verdadeiro impacto da pandemia além dos números oficiais de óbitos por COVID-19.
+        )
 
-    Os dados foram coletados de registros civis, departamentos de saúde e outras fontes oficiais. Como nem todas as mortes relacionadas à COVID-19 foram oficialmente registradas (por falta de testes ou óbitos fora de hospitais), o número de **óbitos em excesso** — diferença entre mortes registradas e o número esperado com base em anos anteriores — oferece um retrato mais completo.
 
-    ### O que você encontrará nesta seção:
-
-    * Dados por país e, em alguns casos, por cidade.
-    * Informações organizadas por semana ou mês.
-    * Comparação entre mortes reais e mortes esperadas.
-    * Base de cálculo construída a partir de médias históricas (2010–2019, dependendo do país).
-    * Tabelas com colunas como: país, localidade, datas, número de óbitos, óbitos esperados e excedentes.
-
-    ### Metodologia:
-
-    * O número de óbitos esperados foi calculado com modelos estatísticos que consideram tendências demográficas e variações sazonais.
-    * As análises podem ser afetadas por atrasos ou falhas nos sistemas de registro, especialmente em países com menor cobertura estatística.
-    * Alguns estados e países foram excluídos por inconsistência ou falta de dados.
-
-    ### Fontes e Licença:
-
-    * Os dados foram coletados e organizados por jornalistas e especialistas do *The New York Times*, com o apoio de pesquisadores e demógrafos.
-    * Estão disponíveis para uso público não comercial, desde que seja feita a devida atribuição ao NYT.
-    * Para mais informações, acesse: [Link para o gráfico interativo original](https://www.nytimes.com/interactive/2020/04/21/world/coronavirus-missing-deaths.html)
-    """
-    )
-
-url = "https://raw.githubusercontent.com/nytimes/covid-19-data/refs/heads/master/excess-deaths/deaths.csv"
-df = pd.read_csv(url)
-
-filtered_df = df
+def load_data():
+    url = "https://raw.githubusercontent.com/nytimes/covid-19-data/refs/heads/master/excess-deaths/deaths.csv"
+    return pd.read_csv(url)
 
 
 def update_filter_by_country():
@@ -63,7 +65,24 @@ def filter_by_country(df: pd.DataFrame):
     return df
 
 
+def sum_deaths_grouped_by(data_frame: pd.DataFrame, group_by: str):
+    return data_frame.groupby(group_by)[deaths_header].sum().reset_index()
+
+
+def handle_data_view(key: str):
+    return st.selectbox(
+        label="Selecione a visão", options=["Gráfico", "Tabela"], key=key
+    )
+
+
+# Main
+df = load_data()
+filtered_df = df
+
+display_about_content()
+
 filtered_df = filter_by_country(df)
+
 default_view, deaths_by_countries, deaths_by_year = st.tabs(
     ["Visão geral", "Mortes por Países", "Mortes por Ano"]
 )
@@ -71,13 +90,9 @@ default_view, deaths_by_countries, deaths_by_year = st.tabs(
 with default_view:
     st.write(filtered_df)
 with deaths_by_countries:
-    grouped_df = filtered_df.groupby(country_header)[deaths_header].sum().reset_index()
+    grouped_df = sum_deaths_grouped_by(filtered_df, country_header)
 
-    view = st.selectbox(
-        label="Selecione a visão",
-        options=["Gráfico", "Tabela"],
-        key="view_by_countries",
-    )
+    view = handle_data_view("view_by_countries")
 
     if view == "Gráfico":
         st.bar_chart(data=grouped_df, x=country_header, y=deaths_header)
@@ -86,11 +101,9 @@ with deaths_by_countries:
 with deaths_by_year:
     filtered_df = filtered_df[filtered_df[year_header].astype(str).str.isdigit()]
 
-    grouped_df = filtered_df.groupby(year_header)[deaths_header].sum().reset_index()
+    grouped_df = sum_deaths_grouped_by(filtered_df, year_header)
 
-    view = st.selectbox(
-        label="Selecione a visão", options=["Gráfico", "Tabela"], key="view_by_year"
-    )
+    view = handle_data_view("view_by_year")
 
     if view == "Gráfico":
         st.line_chart(data=grouped_df, x=year_header, y=deaths_header)
